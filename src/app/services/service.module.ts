192ecfc0e3ea1990e0e76cf42ef3c6e862975a17
LKingJ23 <lkingj23@gmail.com>
2020-06-10 12:15:26 +0200

    login normal y google

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingsService, SidebarService, SharedService } from './service.index';
+import { UsuarioService } from './usuario/usuario.service';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginGuardGuard } from './guards/login-guard.guard';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   providers: [
     SettingsService,
     SidebarService,
-    SharedService
+    SharedService,
+    UsuarioService,
+    LoginGuardGuard
   ]
 })
 export class ServiceModule { }
